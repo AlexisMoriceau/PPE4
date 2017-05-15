f28b77520bf71dd209d7c234f06d363bca8aa620
Alexis <alexis.moriceau@epsi.fr>
2017-05-15 14:48:40 +0200

    Creation des listview

---
+++ PPE/PPE/Pages/Quiz/ListeQuizz.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PPE.Pages.Quiz
+{
+    /// <summary>
+    /// Interaction logic for ListeQuizz.xaml
+    /// </summary>
+    public partial class ListeQuizz : UserControl
+    {
+        public ListeQuizz()
+        {
+            InitializeComponent();
+        }
+
+        private void button_modify_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
