b3cd5f650e56be8531e13eb311579bc09dffce16
Alexis <alexis.moriceau@epsi.fr>
2017-05-15 18:37:31 +0200

    ajout d'interface

---
+++ PPE/PPE/Pages/Sondages/AjoutQuestion.xaml.cs
@@ -0,0 +1,37 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PPE.Pages.Sondages
+{
+    /// <summary>
+    /// Interaction logic for AjoutQuestion.xaml
+    /// </summary>
+    public partial class AjoutQuestion : ModernDialog
+    {
+        public AjoutQuestion()
+        {
+            InitializeComponent();
+
+            // define the dialog buttons
+            this.Buttons = new Button[] { this.OkButton, this.CancelButton };
+        }
+
+        private void textBox_Question_GotFocus(object sender, RoutedEventArgs e)
+        {
+            textBox_Question.Text = "";
+        }
+    }
+}
---
+++ PPE/PPE/Pages/Sondages/AjoutSondage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PPE.Pages.Sondages
+{
+    /// <summary>
+    /// Interaction logic for AjoutSondage.xaml
+    /// </summary>
+    public partial class AjoutSondage : UserControl
+    {
+        public AjoutSondage()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_Nom_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Question_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Reponse_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
