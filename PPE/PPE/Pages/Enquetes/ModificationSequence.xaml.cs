482a972ace17a4e3fd1d2bab7717ce1459ec6480
Alexis <alexis.moriceau@epsi.fr>
2017-05-16 15:35:54 +0200

    page de modif sondage et quiz

---
+++ PPE/PPE/Pages/Enquetes/ModificationSequence.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PPE.Pages.Enquetes
+{
+    /// <summary>
+    /// Interaction logic for ModificationSequence.xaml
+    /// </summary>
+    public partial class ModificationSequence : UserControl
+    {
+        public ModificationSequence()
+        {
+            InitializeComponent();
+        }
+
+        private void button_ModfierQuestion_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_ModifierReponse_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PPE/PPE/Pages/Quiz/ModificationQuiz.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PPE.Pages.Quiz
+{
+    /// <summary>
+    /// Interaction logic for ModificationQuiz.xaml
+    /// </summary>
+    public partial class ModificationQuiz : UserControl
+    {
+        public ModificationQuiz()
+        {
+            InitializeComponent();
+        }
+
+        private void button_ModfierQuestion_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_ModifierReponse_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
