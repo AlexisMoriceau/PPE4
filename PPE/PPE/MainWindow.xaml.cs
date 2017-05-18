0d45aaa0a17c1ef847266f8ece8f2fecc2d48fbb
PutinIsLove <cesar.dindeleux@gmail.com>
2017-05-18 14:14:43 +0200

    Connexion OK

--- PPE/PPE/MainWindow.xaml.cs
+++ PPE/PPE/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace PPE
     {
         public MainWindow()
         {
+
             InitializeComponent();
         }
     }
--- PPE/PPE/Pages/Home.xaml.cs
+++ PPE/PPE/Pages/Home.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FirstFloor.ModernUI.Windows.Controls;
 
 namespace PPE.Pages
 {
@@ -20,9 +21,20 @@ namespace PPE.Pages
     /// </summary>
     public partial class Home : UserControl
     {
+        public ModernWindow window = App.Current.MainWindow as ModernWindow;
+        public FirstFloor.ModernUI.Presentation.LinkGroup aremove0 = (App.Current.MainWindow as ModernWindow).MenuLinkGroups.ElementAt(0);
+
+        public FirstFloor.ModernUI.Presentation.LinkGroup aremove1 = (App.Current.MainWindow as ModernWindow).MenuLinkGroups.ElementAt(1);
+        public FirstFloor.ModernUI.Presentation.LinkGroup aremove2 = (App.Current.MainWindow as ModernWindow).MenuLinkGroups.ElementAt(2);
+        public FirstFloor.ModernUI.Presentation.LinkGroup aremove3 = (App.Current.MainWindow as ModernWindow).MenuLinkGroups.ElementAt(3);
+        public FirstFloor.ModernUI.Presentation.LinkGroup aremove4 = (App.Current.MainWindow as ModernWindow).MenuLinkGroups.ElementAt(4);
         public Home()
         {
             InitializeComponent();
+            window.MenuLinkGroups.Remove(aremove1);
+            window.MenuLinkGroups.Remove(aremove2);
+            window.MenuLinkGroups.Remove(aremove3);
+            window.MenuLinkGroups.Remove(aremove4);
         }
 
         private void TextBox_Login_GotFocus(object sender, RoutedEventArgs e)
@@ -32,6 +44,22 @@ namespace PPE.Pages
 
         private void button_connect_Click(object sender, RoutedEventArgs e)
         {
+            PPE4Entities db = new PPE4Entities();    
+            if (db.Administrateurs.Any(x => x.Login == TextBox_Login.Text && x.Password == PasswordBox.Password))
+            {
+                MessageBox.Show("Bienvenue");
+                window.MenuLinkGroups.Remove(aremove0);
+                window.MenuLinkGroups.Add(aremove1);
+                window.MenuLinkGroups.Add(aremove2);
+                window.MenuLinkGroups.Add(aremove3);
+                window.MenuLinkGroups.Add(aremove4);
+            }
+            else
+            {
+                MessageBox.Show("Ouste");
+            }
+
+
 
         }
     }
